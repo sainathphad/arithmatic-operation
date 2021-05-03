2655499bb5403c6b78550a2092c52a972edfd6a6
sainathphad <phadsainath1@gmail.com>
2021-05-03 19:29:38 +0530

    [sainath] added arithmatic.sh

--- arithmatic.sh
+++ arithmatic.sh
@@ -3,4 +3,6 @@ read -p "Enter number1 " num1
 read -p "Enter number2 " num2
 read -p "Enter number3 " num3
 sum=$(( $num1+$num2+$num3 ))
+substraction=$(( $num1-$num2-$num3 ))
 echo " addition is " $sum
+echo " substraction " $substraction
